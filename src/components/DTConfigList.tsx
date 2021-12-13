88f24e7b8dd8295e9d4aa5ce07b86f8f31c416e6
RaianGil <raian.gdr@gmail.com>
2021-12-13 19:25:00 -0400

    Se modifico el redux y tal

--- src/components/DTConfigList.tsx
+++ src/components/DTConfigList.tsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DTConfig } from "../models/DTConfig";
 import { connect } from 'react-redux'
-import Set from "../controllers/APIData";
+import GETServer from "../store/dispatchs/DTConfigDispatch";
 
 const DTConfigList = ({ConfigList}:any) =>{
-    Set()
-    if (ConfigList.length === 0) return <div>there are no videos yet</div>;
-    return(
-        <div className="">
-            {
-                ConfigList.map((Config:any) => (
-                    <div className="" key={Config._id}>
-                        <label htmlFor="" className="me-2">{Config.CryptoName}</label>
-                        <label htmlFor="" className="me-2">{Config.CryptoSymbol}</label>
-                    </div>
-                ))
-            }
-        </div>
+  GETServer()
+  return(
+      <div className="">
+          {
+              ConfigList.map((Config:DTConfig) => (
+                  <div className="" key={Config._id}>
+                      <label htmlFor="" className="me-2">{Config.CryptoName}</label>
+                      <label htmlFor="" className="me-2">{Config.CryptoSymbol}</label>
+                      <label htmlFor="" className="me-2">{Config.BotMinBuy}</label>
+                      <label htmlFor="" className="me-2">{Config.BotNextBuy}</label>
+                      <label htmlFor="" className="me-2">{Config.AmountLimit}</label>
+                      <label htmlFor="" className="me-2">{Config.BotActive}</label>
+                  </div>
+              ))
+          }
+      </div>
 )}
 
 
-const mapState = (state:any)=> ({
-    ConfigList: state.ConfigDT
-})
+const mapState = async (state:any)=> {
+  alert()
+  return {
+    ConfigList: state.DTConfig.state
+  }
+}
 
 const mapDispatch = (dispatch:any) => ({
 
